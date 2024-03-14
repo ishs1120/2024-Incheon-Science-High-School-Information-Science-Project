aa0d7c4d6077d82d8b18ca3c7d206a1a552d3e93
zigzag <onplaying08@gmail.com>
2024-03-14 20:13:40 +0900

    v.0.1 README.md

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class PrimeNumber {
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        //int count = 0;
+        boolean isPrimeNumber = true;
+        if (number < 2) {
+           //count = 1;
+            isPrimeNumber =false;
+        }
+        else {
+            for (int i = 2; i*i <= number; i = i + 1) {
+                if (number % i == 0) {
+                    //count = count + 1;
+                    isPrimeNumber=false;
+                    break;
+                }
+                System.out.print( i+ "  ");
+            }
+
+
+        }
+        if (isPrimeNumber)
+            System.out.println(number + " is a prime number");
+        else
+            System.out.println(number + " is not a prime number");
+    }
+
 }
---
+++ src/README.md
@@ -0,0 +1,1 @@
+We are sharing the code created in the 2024 Incheon Science High School Information Science Project Research.
