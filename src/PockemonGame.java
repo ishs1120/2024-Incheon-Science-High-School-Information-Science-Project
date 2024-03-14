a5ebd5e66f7de9b3ae2dae730821b53a807281d7
zigzag <onplaying08@gmail.com>
2024-03-14 21:54:19 +0900

    v.0.0 PockemonGame

---
+++ src/PockemonGame.java
@@ -0,0 +1,42 @@
+class Pockemon{
+    private String name;
+    private int hp;
+
+    public Pockemon(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+        System.out.println("parameter construction");
+    }
+
+    public Pockemon() {
+        System.out.println("default construction");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+}
+
+public class PockemonGame {
+    public static void main(String[] args) {
+        Pockemon p2 = new Pockemon("Squirtle", 70);  // parameter constructor
+
+        Pockemon p1 = new Pockemon();  // default consturctor
+        //p1.hp = 50;  // hp has private access in Pokemon
+        p1.setHp(50);
+        p1.setName("Pikachu");
+        System.out.println(p1.getName() + "'s health is " + p1.getHp() + ".");
+    }
+}
