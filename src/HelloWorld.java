d0dea54a40d3c0f3ba1a0fd4f2bb2937048f9315
zigzag <onplaying08@gmail.com>
2024-03-21 17:59:23 +0900

    HelloWorld v.no_maner

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,9 @@ public class HelloWorld {
         Scanner scanner = new Scanner(System.in);
         String name = scanner.next();
         System.out.println("Hello "+name+"!");
+        System.out.println("How can I help you?");
+        String question = scanner.next();
+        System.out.println("I can't understand"+question+"!");
+        System.out.println("I think you are idiot!");
     }
 }
