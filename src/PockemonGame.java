47b4692eb982cd661717bf6ffc28eff93f27f1d4
zigzag <onplaying08@gmail.com>
2024-03-21 20:13:09 +0900

    PockemonGame v.0.3

--- src/PockemonGame.java
+++ src/PockemonGame.java
@@ -1,47 +1,10 @@
-class Pockemon{
-    private String name;
-    private int hp;
-
-    public Pockemon() {
-        this.name = "nameless";
-        this.hp = 10;
-        System.out.println("default constructor");
-    }
-    public Pockemon(String name, int hp) {
-        //name = name;
-        //hp = hp;
-
-        //below code works impairs readability
-        //name= n;
-        //hp =h;
-        this.name = name;
-        this.hp = hp;
-        System.out.println("parameter construction");
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getHp() {
-        return hp;
-    }
-
-    public void setHp(int hp) {
-        this.hp = hp;
-    }
-}
+import Pokemons.Pockemon;
 
 public class PockemonGame {
     public static void main(String[] args) {
         Pockemon p2 = new Pockemon("Squirtle", 70);  // parameter constructor
 
-        Pockemon p1 = new Pockemon();  // default consturctor
-        //p1.hp = 50;  // hp has private access in Pokemon
+        Pockemon p1 = new Pockemon();
         System.out.println(p1.getName() + "'s health is " + p1.getHp() + ".");
         p1.setHp(50);
         p1.setName("Pikachu");
---
+++ src/Pokemons/Pockemon.java
@@ -0,0 +1,32 @@
+package Pokemons;
+public class Pockemon{
+    private String name;
+    private int hp;
+
+    public Pockemon() {
+        this.name = "nameless";
+        this.hp = 10;
+        System.out.println("default constructor");
+    }
+    public Pockemon(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+        System.out.println("parameter construction");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+}
