eb3d2bcd452ad04ac6259544bb4259ac696ee29d
zigzag <onplaying08@gmail.com>
2024-03-28 20:26:09 +0900

    v.1.0 Pokemon Game

--- src/PockemonGame.java
+++ src/PockemonGame.java
@@ -1,3 +1,4 @@
+import Pokemons.Charizard;
 import Pokemons.Pikachu;
 import Pokemons.Pockemon;
 import Pokemons.Squirtle;
@@ -8,10 +9,13 @@ public class PockemonGame {
 
         Pikachu p1 = new Pikachu("Pikachu", 50);
         Squirtle s2 = new Squirtle("Squirtle", 70);
+        Charizard c1 = new Charizard("Charizard", 270);
         p1.info();
         s2.info();
+        c1.info();
         s2.attack(p1);
         p1.attack(s2);
+        c1.attack();
 
 
 
---
+++ src/Pokemons/Charizard.java
@@ -0,0 +1,19 @@
+package Pokemons;
+
+public class Charizard extends Pockemon{
+
+    public Charizard(String name, int hp) {
+        super(name, hp);
+    }
+
+    @Override
+    public void attack() {
+        System.out.println("Wide Area Sirocco");
+    }
+
+    @Override
+    public void attack(Pockemon targetPokemon) {
+        System.out.println(this.getName()+ " attack "+targetPokemon.getName() +" with Uppercase Letters");
+
+    }
+}
