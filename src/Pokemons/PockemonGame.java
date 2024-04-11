9d21ba114d5296cef5b88d1eaa2b1d540f7d0068
ishs1120 <onplaying08@gmial.com>
2024-04-11 21:45:22 +0900

    v.3.1 Pokemon Game

--- src/Pokemons/PockemonGame.java
+++ src/Pokemons/PockemonGame.java
@@ -6,6 +6,23 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class PockemonGame {
+
+    static Pockemon wildPokemon;
+    public static void produceEnemyPokemon(){
+        Random random = new Random();
+
+
+        int select = random.nextInt(3);
+        if(select == 0)
+            wildPokemon = new Pikachu("Pikachu", 50);
+        else if (select == 1)
+            wildPokemon = new Squirtle("Squirtle", 55);
+        else if (select == 2)
+            wildPokemon = new Charizard("Charizard", 74);
+        else
+            wildPokemon = new Pikachu("Pikachu", 50);
+        System.out.println("A wild "+ wildPokemon.getName() +" has appeared");
+    }
     public static void main(String[] args) {
 
         Random random = new Random();
@@ -13,7 +30,7 @@ public class PockemonGame {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Choose your pokemon. 1) Pikachu 2) Squirtle 3) Charizard : ");
         int select = scanner.nextInt();
-        Pockemon playerPokemon, wildPokemon;
+        Pockemon playerPokemon;
         if(select == 1)
             playerPokemon = new Pikachu("Pikachu", 50);
         else if (select == 2) {
@@ -26,21 +43,7 @@ public class PockemonGame {
             playerPokemon = new Pikachu("Pikachu", 50);
 
         System.out.println("You meet wild Pokemon");
-
-
-
-        select = random.nextInt(3);
-        if(select == 1)
-            wildPokemon = new Pikachu("Pikachu", 50);
-        else if(select == 2) {
-            wildPokemon = new Squirtle("Squirtle", 65);
-        }else if(select == 3) {
-            wildPokemon = new Charizard("Charizard", 180);
-        }
-        else{
-            wildPokemon = new Pikachu("Pikachu", 50);
-        }
-        System.out.println("A wild "+ wildPokemon.getName() +" has appeared");
+        produceEnemyPokemon();
 
         //Random random = new Random();
         //random.setSeed(31);
@@ -58,9 +61,12 @@ public class PockemonGame {
                 int skill = scanner.nextInt()-1;
                 playerPokemon.attack(wildPokemon, skill);
                 //wildPokemon.attack(playerPokemon, skill);
+                System.out.println("=====================");
+                wildPokemon.attack(playerPokemon, skill);
             } else if (menu == 2) {
                 System.out.println("Run away");
-                break;
+                playerPokemon.performFly();
+                produceEnemyPokemon();
             }
 
 
